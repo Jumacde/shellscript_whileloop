222acfc6f7df18ed26986d299a4bc27207d5db0a
Yuma <y.takamura0407@gmail.com>
2026-06-26 16:28:09 +0200

    Commited

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,8 +3,11 @@ source "$(dirname "$0")/logging_manager.sh"
 source "$(dirname "$0")/input_num.sh"
 
 main() {
+    # check log dir and choose one.
+    # command: sudo ./main.sh -> test_log
+    # command: sudo ENV=prod ./main.sh -> prod_log
 	get_logdir
-
+    # execute function and save result to log file.
     inputnum >> "$log_dir"
 }
 main
