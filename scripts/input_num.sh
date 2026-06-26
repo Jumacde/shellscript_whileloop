6206d0d06b26e6a2a7b4049a4559d96ab734d97a
Yuma <y.takamura0407@gmail.com>
2026-06-26 21:39:17 +0200

    edit comment

--- scripts/input_num.sh
+++ scripts/input_num.sh
@@ -6,15 +6,17 @@ inputnum() {
 	#local num
 	#read -p "input a digit: " num
 	#echo "You entered: $num"
+
+	# when you input 0 stop this script. anycase show message.
 	while [ "$num" != "0" ]
 	do 
 		echo "please input a digit. find a digit to end this programm."
 		read num
 		echo "you inputed: $num" | tee -a "$log_dir"
 		case "$num" in
-			1) echo ": one";;
-			2) echo ": two";;
-			3) echo ": three";;
+			1) echo ": no";;
+			2) echo ": noo";;
+			3) echo ": nooo";;
 			0) echo ": bye"
 				exit;;
 			*) echo ": others";;
